6969beb9d248358b61ee3553124dcc979daa0e51
Pia Schoknecht <Schoknec@students.uni-marburg.de>
2014-05-05 12:35:36 +0200

    z Transformation

---
+++ sitzung_05_05.R
@@ -0,0 +1,11 @@
+rtdata <- read.table("Schoknec/priming_rt.tab",header = T)
+summary(rtdata)
+print(rtdata)
+rtdata$subj <- as.factor(rtdata$subj)
+rtdata[1,]
+#Zentrieren
+rt.zentriert <- rtdata$RT - mean (rtdata$RT)
+head(rt.zentriert, n=4)
+#z-Transformation
+rt.z <- (rtdata$RT - mean (rtdata$RT)) / sd (rtdata$RT)
+head(rt.z, n=4)
